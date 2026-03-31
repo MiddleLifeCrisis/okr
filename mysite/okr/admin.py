42d8e0a2aa774a250cd0611cf56895c99daa5ac0
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-31 15:12:51 +0300

    v2 (istryniau viska kas buvo, kad galeciau mokytis nuo 0) - modeliai sukurti, dar be migraciju ir actions modelis neaprasytas

--- mysite/okr/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Objective, KeyResult, Initiative, KPI
-
-admin.site.register(KPI)
-admin.site.register(Objective)
-admin.site.register(KeyResult)
-admin.site.register(Initiative)
