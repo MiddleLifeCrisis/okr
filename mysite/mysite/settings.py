3eed544ff32185f2f9911985f3f8bd12065cbd14
Justas Osipovas <justas.osipovas@gmail.com>
2026-05-11 14:39:10 +0300

    settings.py.txt +STATIC_ROOT = '/home/Osipovas/okr/mysite/staticfiles/'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/Osipovas/okr/mysite/staticfiles/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
